3dae27d064b6964f6fc60ed164b359ac0572d4fe
Matin-Modarresi <58595108+Matin-Modarresi@users.noreply.github.com>
2020-09-11 20:10:14 +0430

    Balance capacity and size in vector with shrink_to_fit

--- vector_8_shrink_to_fit.cpp
+++ vector_8_shrink_to_fit.cpp
@@ -7,18 +7,18 @@ int main()
 {
 	vector<int> v(6);
 	
-	cout << v.size()  << "\n";
-	cout << v.capacity() << "\n";
+	cout << v.size()  << "\n";//6
+	cout << v.capacity() << "\n";//6
 	
 	cout << "resize-----------------\n";
     v.resize(3);
-    cout << v.size() << "\n";
-    cout << v.capacity() << "\n";
+    cout << v.size() << "\n";//3
+    cout << v.capacity() << "\n";//6
     
     cout << "use shrink_to_fit-------------------\n";
     
     v.shrink_to_fit();
-    cout << v.size() << "\n";
-    cout << v.capacity() << "\n";
+    cout << v.size() << "\n";//3
+    cout << v.capacity() << "\n";//3
     
 }
